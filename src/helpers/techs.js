dde37b74c85ae7893b6bdacc966cbb70a80c1bbc
durga-04 <borradurgasree8@gmail.com>
2023-11-28 15:17:38 +0530

    added navigation bar

---
+++ src/helpers/techs.js
@@ -0,0 +1,38 @@
+export const techs = [
+  {
+    name: 'React',
+    iconName: 'react'
+  },
+  {
+    name: 'Ruby',
+    iconName: 'ruby'
+  },
+  {
+    name: 'Javascript',
+    iconName: 'javascript'
+  },
+  {
+    name: 'CSS3',
+    iconName: 'css3'
+  },
+  {
+    name: 'HTML 5',
+    iconName: 'html5'
+  },
+  {
+    name: 'Mongo DB',
+    iconName: 'mongodb'
+  },
+  {
+    name: 'Bootstrap',
+    iconName: 'bootstrap'
+  },
+  {
+    name: 'SQL lite',
+    iconName: 'sqllite'
+  },
+  {
+    name: 'Github',
+    iconName: 'github'
+  },
+]
